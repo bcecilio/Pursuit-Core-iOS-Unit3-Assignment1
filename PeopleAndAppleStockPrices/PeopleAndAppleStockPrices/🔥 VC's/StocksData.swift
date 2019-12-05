a8c238981d49de9c1eff07b2cd786e6e7fcd0ae3
brendon cecilio <brendon.cecilio@gmail.com>
2019-12-04 22:54:15 -0500

    safety push

---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/🔥 VC's/StocksData.swift
@@ -0,0 +1,30 @@
+//
+//  StocksData.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by Brendon Cecilio on 12/3/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+struct StocksData: Decodable {
+    let date: String
+    let uOpen: Double
+}
+
+extension StocksData {
+    static func getStockInfo(from data: Data) -> [StocksData] {
+        var stocks = [StocksData]()
+        
+        do {
+            let stockData = try JSONDecoder().decode([StocksData].self, from: data)
+            stocks = stockData
+        } catch {
+            fatalError("failed to load contents \(error)!!")
+        }
+        return stocks
+    }
+}
+
+
