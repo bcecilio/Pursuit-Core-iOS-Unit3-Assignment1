46c9b436472a8962ac2fb8fb591ff21a62b2048b
brendon cecilio <brendon.cecilio@gmail.com>
2019-12-05 09:13:23 -0500

    StockVC complete

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/🔥 VC's/StocksViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/🔥 VC's/StocksViewController.swift
@@ -11,10 +11,35 @@ import UIKit
 class StocksViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var stockInfo = [StocksData](){
+        didSet {
+            tableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//        tableView.dataSource = self
     }
+    
+    func loadData() {
+        stockInfo = StocksData.getStockInfo(from: Data.init())
+    }
+}
 
+extension StocksViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stockInfo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "stockCell", for: indexPath)
+        
+        let stockCell = stockInfo[indexPath.row]
+        cell.textLabel?.text = stockCell.date
+        return cell
+    }
+    
+    
 }
